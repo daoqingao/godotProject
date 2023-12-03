875c708acba86075946a9ebd3a8c05709a1e3423
daoqingao <daoqingao@gmail.com>
2023-12-03 16:51:22 -0500

    client only servers

--- CardGameClient/scripts/ConnectionManager.gd
+++ CardGameClient/scripts/ConnectionManager.gd
@@ -13,3 +13,14 @@ func _process(delta):
 func _on_server_do_something_pressed():
 	server.askServer()
 	return;
+
+
+func _on_join_pressed():
+	server.joinServer()
+	pass # Replace with function body.
+
+
+func _on_host_pressed():
+	print("host button created, this instance is now the server and should not play")
+	server.startServer()
+	pass # Replace with function body.
--- CardGameClient/server/server.gd
+++ CardGameClient/server/server.gd
@@ -1,52 +1,96 @@
 extends Node2D
+##################SHARED SCHEMAS>?
 
 const MAX_CONNECTIONS = 20
 const SERVER_ADDRESS = "127.0.0.1" # IPv4 localhost
 const SERVER_PORT = 7000
-func Client_Join_Server() -> void:
+func _ready():
+	#deployed server would only just call
+	#startServer() #immediately on run
+	return
+	
+#server and client
+func _on_server_receive_client_connect(id):
+	print("server signals to client: client connected from the server",id)
+func _on_server_receive_client_disconnect(id):
+	print("server signals to client: client disconnected from the server",id)
+
+
+
+
+
+###################################### SERVER ONLY
+var serverPlayers = {}
+
+func startServer():
+	print("this instance is now the server. and should not be playing ")
+	var peer = ENetMultiplayerPeer.new()
+	var error = peer.create_server(SERVER_PORT, MAX_CONNECTIONS)
+	if(error):
+		print("server: server was not able to start successfully", str(error))
+	multiplayer.multiplayer_peer = peer #means that we make the 
+	multiplayer.peer_connected.connect(_on_server_receive_client_connect)
+	multiplayer.peer_disconnected.connect(_on_server_receive_client_disconnect)
+	print("server: server started")
+
+# server calls the client.
+#any peer can send data to server
+@rpc("any_peer","call_remote")
+func SendClientDataToServer(playerData,actionType):
+	if actionType=="connected":
+		var playerId = playerData
+		print("the server should be getting this. ", playerId)
+		if !serverPlayers.has(playerId):
+			serverPlayers[playerId] = {
+				playerId = playerId
+			}
+		SendServerPlayerDataToClient.rpc(serverPlayers)
+
+	
+	
+##################################### client
+
+var localPlayers = {}
+func _on_client_success_connect_to_server():
+	print("client: successfully connect to server")
+	SendClientDataToServer.rpc_id(1, {
+		playerId = multiplayer.get_unique_id()
+	},"connected")
+func _on_client_fail_connect_to_server():
+	print("client: unable to connect to server")
+func _on_client_disconnect_to_server():
+	print("client: disconnected to server")
+	
+func joinServer():
 	var peer: ENetMultiplayerPeer = ENetMultiplayerPeer.new()
 	if peer.create_client(SERVER_ADDRESS, SERVER_PORT):
 		printerr("Error creating the client")
 	multiplayer.multiplayer_peer = peer 
-func _ready():
 	multiplayer.peer_connected.connect(_on_server_receive_client_connect)
 	multiplayer.peer_disconnected.connect(_on_server_receive_client_disconnect)
 	multiplayer.connected_to_server.connect(_on_client_success_connect_to_server)
 	multiplayer.connection_failed.connect(_on_client_fail_connect_to_server)
 	multiplayer.server_disconnected.connect(_on_client_disconnect_to_server)
-	Client_Join_Server()
-
-func _on_server_receive_client_connect(id):
-	print("server signals to client: client connected from the server",id)
-func _on_server_receive_client_disconnect(id):
-	print("server signals to client: client disconnected from the server",id)
-func _on_client_success_connect_to_server():
-	print("client: successfully connect to server")
-	SendClientDataToServer.rpc_id(1, multiplayer.get_unique_id())
-func _on_client_fail_connect_to_server():
-	print("client: unable to connect to server")
-func _on_client_disconnect_to_server():
-	print("client: disconnected to server")
-
+	return
 
 
 func askServer():
-	print("trying to call server to do this")
+	#ask the server to start game bruh
 	SendClientDataToServer.rpc_id(1, {
 		playerId = multiplayer.get_unique_id()
-	},"connected")
-	
+	},"start")
+	print("trying to call server to do this")
 
-#### 
-# server calls the client.
-@rpc()
+	
+	
+#authority means the server sends data back to
+@rpc("authority","call_remote")
 func SendServerPlayerDataToClient(players):
-	print("got data from server. ", players)
-#########################################
-#all server interface callable functions.
-# this also represents the server schema....... bruh
-@rpc()
-func SendClientDataToServer(playerData,actionType):
+	print("client should be getting this back from the server", players)
+	localPlayers = players
 	pass
+
+
+
 
 
